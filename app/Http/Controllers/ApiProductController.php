c8c0bae070feb7110e6d4c937c7b1799589a29c9
jppacinos <jp.pacinos@gmail.com>
2023-05-04 19:25:02 +0800

    initial products rest apis

---
+++ app/Http/Controllers/ApiProductController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ApiProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return response()->json(
+            Product::paginate(
+                $request->query('per_page', 15)
+            )->withQueryString()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:255|string',
+            'description' => 'required|min:3|string',
+            'price' => 'required|numeric|min:1|max:99999999|decimal:2',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json([
+            'message' => 'Success',
+            'data' => [
+                'product' => $product,
+            ],
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validated = $request->validate([
+            'name' => 'min:3|max:255|string',
+            'description' => 'min:3|string',
+            'price' => 'numeric|min:1|max:99999999|decimal:2',
+        ]);
+
+        $product->update($validated);
+
+        return response()->json([
+            'message' => 'Success',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response('', 204);
+    }
+}
